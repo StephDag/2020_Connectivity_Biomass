36786b4f0a9398a7ddeb23da30683effa2005ab5
Joseph Maina <mainajm@gmail.com>
2020-12-22 11:29:00 +1100

    #uploading grids

---
+++ wiosubsetanal.R
@@ -0,0 +1,14 @@
+library(rgdal)
+library(raster)
+mypath<-"/Users/josephmaina/Documents/Mygitprojects/2020_Connectivity_Biomass/_data/reefGrids/"
+
+reef.grids<-readOGR(dsn=mypath, "settlement")
+
+wiompagdb<-"~/Dropbox/WIO_MPA_Outlook_Maps_Database/GeoDatabase/WIO_MPA_Outlook_Feb2020.gdb"
+subset(ogrDrivers(), grepl("GDB", name))
+fc_list <- ogrListLayers(wiompagdb)
+print(fc_list)
+
+#wiompa<-readOGR(dsn=wiompagdb, "WIO_MPAs_Combined",require_geomType = 'wkbPolygon')
+require(sf)
+wiompa <- sf::st_read(wiompagdb, layer = "WIO_MPAs_Combined")
